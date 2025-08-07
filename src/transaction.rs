41a5de88736d86f75f041e16bac6cbb92300357f
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-07 22:49:38 +0100

    feat: paste the rust code from main to transaction

---
+++ src/transaction.rs
@@ -0,0 +1,49 @@
+use serde::{Serialize, Serializer};
+
+#[derive(Debug, Serialize)]
+
+pub struct Transaction {
+    pub version: u32,
+    pub inputs: Vec<Input>,
+    pub outputs: Vec<Output>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Input {
+    pub txid: String,
+    pub output_index: u32,
+    pub script_sig: String,
+    pub sequence: u32,
+}
+
+#[derive(Debug)]
+
+pub struct Amount( u64);
+
+impl Amount {
+    pub fn from_sat(satoshi: u64) -> Amount {
+        Amount(satoshi)
+    }
+}
+
+trait BitcoinValue {
+    fn to_btc(&self) -> f64;
+}
+
+impl BitcoinValue for Amount {
+    fn to_btc(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Output {
+    #[serde(serialize_with = "as_btc")]
+    pub amount: Amount,
+    pub script_pubkey: String,
+}
+
+fn as_btc<S: Serializer, T: BitcoinValue>(t: &T, s: S) -> Result<S::Ok, S::Error> {
+    let btc_value = t.to_btc();
+    s.serialize_f64(btc_value)
+}
